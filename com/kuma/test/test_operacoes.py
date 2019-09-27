e03bd8c97b7e291383371d000d3282515d6b06dd
Gerson Callisaya Gonzales <1900832@SALAS.AULAS>
2019-09-27 20:00:55 -0300

    Tirando os prints

--- com/kuma/test/test_operacoes.py
+++ com/kuma/test/test_operacoes.py
@@ -1,10 +1,6 @@
-from unittest import TestCase
+import pytest
 from com.kuma.operacoes import Operacoes
 
-class TestOperacoes(TestCase):
-
-	def setUp(self):
-		self.operacoes = Operacoes()
-		
-	def test_soma(self):
-		self.assertEqual(self.operacoes.soma([1,5]), 6, "Should be 6")
+def test_soma():
+	operacoes = Operacoes()
+	assert operacoes.soma([1,5]) == 6, "Should be 6"
